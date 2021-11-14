6a3d7ae9354a5890cd74f86a793214230acab4fc
OwnKng <owen.king@outlook.com>
2021-11-14 14:27:03 +0000

    Preping deploy

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -1,34 +1,24 @@
-const { query } = require('./util/hasura');
+const { query } = require("./util/hasura")
 
-exports.handler = async (event, context) => {
-  const { id, title, tagline, poster } = JSON.parse(event.body);
-  const { user } = context.clientContext;
-  const isLoggedIn = user && user.app_metadata;
-  const roles = user.app_metadata.roles || [];
-
-  if (!isLoggedIn || !roles.includes('admin')) {
-    return {
-      statusCode: 401,
-      body: 'Unauthorized',
-    };
-  }
+exports.handler = async (event) => {
+  const { id, title, tagline, poster } = JSON.parse(event.body)
 
   const result = await query({
     query: `
-      mutation ($id: String!, $poster: String!, $tagline: String!, $title: String!) {
-        insert_movies_one(object: {id: $id, poster: $poster, tagline: $tagline, title: $title}) {
-          id
-          poster
-          tagline
-          title
-        }
+    mutation MyMutation($id: String!, $poster: String!, $tagline: String!, $title: String!) {
+      insert_movies_one(object: {id: $id, tagline: $tagline, title: $title, poster: $poster}) {
+        id
+        poster
+        tagline
+        title
       }
+    }
     `,
     variables: { id, title, tagline, poster },
-  });
+  })
 
   return {
     statusCode: 200,
     body: JSON.stringify(result),
-  };
-};
+  }
+}
